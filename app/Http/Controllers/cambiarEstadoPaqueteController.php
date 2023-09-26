a12ef53e4a4f31ab75daa70e74aa8e43b07f43e2
Matias Meneses <mat.meneses47@gmail.com>
2023-09-26 11:40:54 -0300

    Pruebas unitarias de Modificar el estado del paquete a entregado realizadas y funcionales

--- app/Http/Controllers/cambiarEstadoPaqueteController.php
+++ app/Http/Controllers/cambiarEstadoPaqueteController.php
@@ -5,22 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Estados_p;
 use App\Models\Paquetes;
+use Illuminate\Support\Facades\Validator;
+
 class cambiarEstadoPaqueteController extends Controller
 {
     public function buscarPaquete(Request $request){
-        $paquete=Paquetes::withoutTrashed()->where('id',$request->input('id'))->first();
-        if($paquete){
-        $this->cambiarEstadoCamion($paquete);
+        $validador = $this->validarDatos($request->all());
+        if ($validador->fails()) {
+            return;
         }
+        $paquete=Paquetes::withoutTrashed()->where('id',$request->post('id'))->first();
+        if($paquete){
+        $respuesta=$this->cambiarEstadoPaquete($paquete);
+        return response()->json(['message' => $respuesta]);
+        }
+        return response()->json(['message' => 'No hay ningún paquete con esta id']);
+        
     }
 
-    public function cambiarEstadoCamion($paquete){
-        $estado=Estados_p::withTrashed()->where('id',$paquete['id_estado_p'])->first();
-        if($estado['descripcion']!='entregado'){
-            $nuevoEstado=Estados_p::withoutTrashed()->where('descripcion','entregado')->first();
+    public function cambiarEstadoPaquete($paquete){
             $paquete->update([
-                'id_estado_p'=>$nuevoEstado['id']
+                'id_estado_p'=>'3'
             ]);
-        }
+            return 'Paquete modificado con exito';
     }
+
+    private function validarDatos($request)
+    {
+
+        $reglas = [
+            'id' => 'required|integer',
+        ];
+        return Validator::make([
+            'id' => $request['id'],
+        ], $reglas);
+    }
+
 }
