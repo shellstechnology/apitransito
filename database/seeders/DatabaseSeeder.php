6c46a9eeb4c46eff10629065a89c0fa451dc0f0d
Matias Meneses <mat.meneses47@gmail.com>
2023-11-04 20:07:16 -0300

    cambios database seeder actions otra vez

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,9 +29,9 @@ class DatabaseSeeder extends Seeder
         $this->call(CamionesSeeder::class);
         $this->call(Camion_Lleva_LoteSeeder::class);
         $this->call(UsuariosSeeder::class);
-        $this->call(telefonos_usuariosSeeder::class);
-        $this->call(mail_UsuariosSeeder::class);
+        $this->call(Telefonos_UsuariosSeeder::class);
+        $this->call(Mail_UsuariosSeeder::class);
         $this->call(ChoferesSeeder::class);
-        $this->call(chofer_conduce_camionSeeder::class);
+        $this->call(Chofer_Conduce_CamionSeeder::class);
     }
 }
